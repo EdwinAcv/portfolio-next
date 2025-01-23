bdfa0ab30bb9d12e30a28106277fe7aa4f6f1c5f
edwin <edwincorreog@gmail.com>
2025-01-22 20:48:23 -0400

    Ultimos cambios agregados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,20 @@
 
 export default function Home() {
   return (
-      <h1>Hola mundo!!</h1>
+  <div className="grid grid-cols-2 gap-4">
+    <div>
+      <h1 className="text-[5rem] leading-[4rem]">Full Stack Developer</h1>
+
+      <br/>
+      <div className="bg-[#F5F4D7] rounded-[10px] p-4">
+        <h3> Descargar CV</h3>
+      </div>
+      {/* <button> Descargar CV </button> */}
+    </div>
+    
+    <div className="p-[200px] bg-fuchsia-500">
+      a
+    </div>
+  </div>
   );
 }
