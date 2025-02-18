e5d3e24703136cdab7992c2b003cab76146a7695
edwin <edwincorreog@gmail.com>
2025-02-17 22:43:32 -0400

    Trabajando con el re-render en el navbar

--- src/components/NabBar.tsx
+++ src/components/NabBar.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import { NavBarMenuItem } from './NavBarMenuItem';
 import Image from 'next/image';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 const rutas = [
     {
@@ -26,21 +28,56 @@ const rutas = [
 ]
 
 export const NabBar = () => {
+    // console.log('nav');
+    const searchParams = useSearchParams();
+    const param = searchParams.get("section") || "aboutme"; // Si no hay `section`, usa `aboutme`
+    
+    const router = useRouter();
+    const [activeSection, setActiveSection] = useState(param);
+
+    // Hacer scroll suave cuando cambia el query param
+    useEffect(() => {
+        const sectionElement = document.getElementById(param);
+        if (sectionElement) {
+            sectionElement.scrollIntoView({ behavior: "smooth" });
+        }
+    }, [param]);
+
+    useEffect(() => {
+        const sections = document.querySelectorAll("section"); // Seleccionar todas las secciones
+        const observer = new IntersectionObserver(
+            (entries) => {
+                const visibleSection = entries.find((entry) => entry.isIntersecting);
+                if (!visibleSection) return; 
+
+                const newSection = visibleSection.target.id;
+                if (!(newSection !== activeSection)) return;
+                
+                setActiveSection(newSection);
+                
+                // router.push(`?sectione=${newSection}`, { scroll: false });
+            }, { threshold: 0.6 } // Se activa cuando el 60% de la sección es visible
+        );
+        sections.forEach((section) => observer.observe(section));
+    
+        return () => observer.disconnect(); // Limpiar el observer al desmontar
+
+    }, [activeSection]);
+    
+    useEffect(() => {
+        const param = searchParams.get("section") || "aboutme"; // Si no hay `section`, usa `aboutme`
+         router.push(`?section=${param}`, { scroll: false });// toma la seccion actual cuando se recarga la pagina
+    }), []
 
   return (
     <div>
-        {/* <nav className="fixed top-0 left-0 w-full bg-white shadow-md p-4 flex justify-center space-x-6">
-              <a href="#aboutme" className="text-blue-600 hover:underline">Inicio</a>
-              <a href="#skills" className="text-blue-600 hover:underline">Servicios</a>
-              <a href="#projects" className="text-blue-600 hover:underline">Contacto</a>
-            </nav> */}
         <nav className='bg-navBar sm:fixed top-0 left-0 w-full z-[15] sm:flex  justify-between items-center'>
             <div className='p-2'>
                 <Image className='hover:cursor-pointer size-[40px]' src={'/logo.svg'} alt='logo' width={0} height={0}/>
             </div>
             <ul className='sm:flex gap'>
                 {rutas.map((ruta) => (
-                     <NavBarMenuItem key={ruta.path} {...ruta}/>
+                     <NavBarMenuItem activeSection={activeSection} key={ruta.path} {...ruta}/>
                 ))}
             </ul>
         </nav>
--- src/components/NavBarMenuItem.tsx
+++ src/components/NavBarMenuItem.tsx
@@ -1,53 +1,12 @@
-"use client";
-
 import Link from "next/link";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
 
 interface IRuta {
   label: string;
   path: string;
+  activeSection: string;
 }
 
-export const NavBarMenuItem = ({ label, path }: IRuta) => {
-  const searchParams = useSearchParams();
-  const param = searchParams.get("section") || "aboutme"; // Si no hay `section`, usa `aboutme`
-
-  const router = useRouter();
-  const [activeSection, setActiveSection] = useState(param);
-
-  // 📌 Hacer scroll suave cuando cambia el query param
-  useEffect(() => {
-    const sectionElement = document.getElementById(param);
-    if (sectionElement) {
-        sectionElement.scrollIntoView({ behavior: "smooth" });
-    }
-  }, [param]);
-
-  // 📌 Detectar scroll manualmente y actualizar la URL
-  useEffect(() => {
-    const sections = document.querySelectorAll("section"); // Seleccionar todas las secciones
-    const observer = new IntersectionObserver(
-        (entries) => {
-            const visibleSection = entries.find((entry) => entry.isIntersecting);
-            if (visibleSection) {
-                const newSection = visibleSection.target.id;
-                if (newSection !== activeSection) {
-                    setActiveSection(newSection);
-                }
-            }
-        },
-        { threshold: 0.6 } // Se activa cuando el 60% de la sección es visible
-    );
-
-    sections.forEach((section) => observer.observe(section));
-
-    return () => observer.disconnect(); // Limpiar el observer al desmontar
-}, [activeSection]);
-  const push = (pa: string) => {
-    router.push(`?section=${pa}`, { scroll: false });
-  };
-
+export const NavBarMenuItem = ({ label, path, activeSection }: IRuta) => {
   return (
     <Link href={`?section=${path}`} scroll={false}>
       <div
